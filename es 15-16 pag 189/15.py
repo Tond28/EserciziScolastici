300db736e34996434753e30b9d67c5a34cc9fe9e
Tond28 <filippotondelli@gmail.com>
2021-01-27 19:26:07 +0100

    add /es 15-16 pag 189

---
+++ es 15-16 pag 189/15.py
@@ -0,0 +1,25 @@
+#Dato un elenco di nazioni con le rispettive capitali, 
+#Inserito il nome di una nazione restituire la capitale
+#Se non è presente la nazione dare un messaggio di errori
+#Utilizzare 2 liste
+
+nazioni=["Italia", "Spagna", "Germania", "Regno unito", "Portogallo"]
+capitali=["Roma", "Madrid", "Berlino", "Londra", "Lisbona"]
+
+while True:
+    nazione=input("Inserire una nazione (inserire aaa per fermare il programma) \n")
+    nazione=(nazione.lower()).capitalize()
+    if nazione=="Aaa":
+        break
+    elif nazione in nazioni:
+        print("La capitale di", nazione, "è", capitali[nazioni.index(nazione)])
+    else:
+        #print("Nazione non presente")
+        #aggiunta
+        print("Inserire la capitale di", nazione)
+        capitale=input()
+        capitale=(capitale.lower()).capitalize()
+        nazioni.append(nazione)
+        capitali.append(capitale)
+        print("Nazione e Capitale aggiunta correttamente")
+        
---
+++ es 15-16 pag 189/16.py
@@ -0,0 +1,31 @@
+#Dato un elenco di nazioni con le rispettive capitali, 
+#Inserito il nome di una nazione restituire la capitale
+#Se non è presente la nazione dare un messaggio di errori
+#Utilizza una dictionary
+
+nazioni_capitali={
+    "Italia":"Roma",
+    "Francia":"Parigi",
+    "Spagna":"Madrid",
+    "Germania":"Berlino",
+    "Regno unito":"Londra",
+    "Portogallo":"Lisbona"
+}
+
+while True:
+    nazione=input("Inserire una nazione (inserire aaa per fermare il programma) \n")
+    nazione=(nazione.lower()).capitalize()
+    if nazione=="Aaa":
+        break
+    elif nazione in nazioni_capitali:
+        print("La capitale di", nazione, "è", nazioni_capitali[nazione])
+    else:
+        #print("Nazione non presente")
+        #aggiunta
+        print("Inserire la capitale di", nazione)
+        capitale=input()
+        capitale=(capitale.lower()).capitalize()
+        nazioni_capitali[nazione]=capitale
+        print("Nazione e Capitale aggiunta correttamente")
+        print(nazioni_capitali)
+    
