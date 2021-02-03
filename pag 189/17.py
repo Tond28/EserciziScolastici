8ee9700d00fb0a3103073135489d64bd07dc2403
Tond28 <filippotondelli@gmail.com>
2021-02-03 16:21:37 +0100

    added pag 189/17.py

---
+++ pag 189/17.py
@@ -0,0 +1,26 @@
+nazioni_capitali={
+    "Italia":"Roma",
+    "Francia":"Parigi",
+    "Spagna":"Madrid",
+    "Germania":"Berlino",
+    "Regno unito":"Londra",
+    "Portogallo":"Lisbona"
+}
+capitali_nazioni={capitali: nazioni for nazioni, capitali in nazioni_capitali.items()}
+while True:
+    capitale=input("Inserire una capitale (inserire aaa per fermare il programma) \n")
+    capitale=capitale.capitalize()
+    if capitale=="Aaa":
+        break
+    elif capitale in capitali_nazioni:
+        print(capitale, "è in", capitali_nazioni[capitale])
+    else:
+        #print("Nazione non presente")
+
+
+        #aggiunta
+        print("Inserire la nazione di", capitale)
+        nazione=input()
+        nazione=nazione.capitalize()
+        capitali_nazioni[capitale]=nazione
+        print("Nazione e Capitale aggiunta correttamente")
