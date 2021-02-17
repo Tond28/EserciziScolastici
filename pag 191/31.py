a243aa14b7ec51cb443b2932fe7fc45ef8a939ec
Tond28 <filippotondelli@gmail.com>
2021-02-17 18:12:27 +0100

    fix text

--- pag 191/31.py
+++ pag 191/31.py
@@ -1,5 +1,5 @@
 #Un'azienda vende prodotti in tutta Italia, e la rete di vendità è suddivisa in Nord, Centro, Sud, Isole. Dopo aver acquisito in un array il fatturato
-#della quattro zone, calola: il totale del fatturato, i valori eprcentuali delle vendite nelle quattro zone rispetto al totale
+#della quattro zone, calola: il totale del fatturato e i valori percentuali delle vendite nelle quattro zone rispetto al totale
 
 zone={
     "nord":0,
